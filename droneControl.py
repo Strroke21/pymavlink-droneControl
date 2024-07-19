5d3d43c2cc9d150d42e56369be21f08bb9b19f7a
Strroke21 <rishipatil1707@gmail.com>
2024-07-19 11:43:51 +0530

    new

---
+++ droneControl.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+from pymavlink import mavutil
+
+def connect(connection_string):
+
+    vehicle =  mavutil.mavlink_connection(connection_string)
+
+    return vehicle
+
+def VehicleMode(mode_id,vehicle):
+
+    ##### changing to guided mode #####
+    #mode_id = 0:STABILIZE, 1:ACRO, 2: ALT_HOLD, 3:AUTO, 4:GUIDED, 5:LOITER, 6:RTL, 7:CIRCLE, 9:LAND
+    vehicle.mav.set_mode_send(
+        vehicle.target_system,mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,mode_id)
+    
+
+def arm(vehicle):
+    #arm the drone
+    vehicle.mav.command_long_send(vehicle.target_system, vehicle.target_component,
+    	                                 mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
+    
+def drone_takeoff(vehicle, altitude):
+    
+    # Send MAVLink command to takeoff
+    vehicle.mav.command_long_send(
+        vehicle.target_system,       # target_system
+        vehicle.target_component,    # target_component
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,  # command
+        0,                          # confirmation
+        0,                          # param1 (min pitch, not used)
+        0,                          # param2 (empty for now, not used)
+        0,                          # param3 (empty for now, not used)
+        0,                          # param4 (yaw angle in degrees, not used)
+        0,                          # param5 (latitude, not used)
+        0,                          # param6 (longitude, not used)
+        altitude                    # param7 (altitude in meters)
+    )
+
+
+def send_velocity_setpoint(vehicle, vx, vy, vz):
+
+    # Send MAVLink command to set velocity
+    vehicle.mav.set_position_target_local_ned_send(
+        0,                          # time_boot_ms (not used)
+        vehicle.target_system,       # target_system
+        vehicle.target_component,    # target_component
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # frame
+        0b0000111111000111,        # type_mask (only vx, vy, vz, yaw_rate)
+        0, 0, 0,                    # position (not used)
+        vx, vy, vz,                 # velocity in m/s
+        0, 0, 0,                    # acceleration (not used)
+        0, 0                        # yaw, yaw_rate (not used)
+    )
+ 
+def get_local_position(vehicle):
+    vehicle.wait_heartbeat()
+    vehicle.mav.request_data_stream_send(
+    vehicle.target_system, 
+    vehicle.target_component,
+    mavutil.mavlink.MAV_DATA_STREAM_POSITION,
+    100,1)
+    msg = vehicle.recv_match(type='LOCAL_POSITION_NED', blocking=True)
+    pos_x = msg.x # Degrees
+    pos_y = msg.y  # Degrees
+    pos_z = msg.z  # Meters
+    return [pos_x,pos_y,pos_z]
+
+#target waypoint function
+
+def goto_waypoint(vehicle,latitude, longitude, altitude):
+    msg = vehicle.mav.set_position_target_global_int_encode(
+        time_boot_ms=10,
+        target_system=vehicle.target_system,       # Target system (usually 1 for drones)
+        target_component=vehicle.target_component,    # Target component (usually 1 for drones)
+        coordinate_frame=mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,  # Frame of reference for the coordinate system
+        type_mask=0b0000111111111000,        # Bitmask to indicate which dimensions should be ignored (0b0000111111111000 means all ignored except position)
+        lat_int=int(latitude * 1e7),       # Latitude in degrees * 1e7 (to convert to integer)
+        lon_int=int(longitude * 1e7),      # Longitude in degrees * 1e7 (to convert to integer)
+        alt=altitude * 1000,           # Altitude in meters (converted to millimeters)
+        vx=0,                         # X velocity in m/s (not used)
+        vy=0,                         # Y velocity in m/s (not used)
+        vz=0,                         # Z velocity in m/s (not used)
+        afx=0, afy=0, afz=0,                   # Accel x, y, z (not used)
+        yaw=0, yaw_rate=0                       # Yaw and yaw rate (not used)
+    )
+    vehicle.mav.send(msg)
+
+
+def get_global_position(vehicle):
+    vehicle.wait_heartbeat()
+    vehicle.mav.request_data_stream_send(
+    vehicle.target_system, 
+    vehicle.target_component,
+    mavutil.mavlink.MAV_DATA_STREAM_POSITION,
+    100,1)
+    msg = vehicle.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
+    lat = msg.lat/1e7 # lat
+    lon = msg.lon/1e7 # lon
+    alt = msg.alt/1000  # alt
+    return [lat,lon,alt]
+
+def send_position_setpoint(vehicle, pos_x, pos_y, pos_z):
+
+    # Send MAVLink command to set velocity
+    vehicle.mav.set_position_target_local_ned_send(
+        0,                          # time_boot_ms (not used)
+        vehicle.target_system,       # target_system
+        vehicle.target_component,    # target_component
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # frame
+        0b0000111111000111,        # type_mask (only vx, vy, vz, yaw_rate)
+        pos_x, pos_y, pos_z,   # position 
+        0, 0, 0,                 # velocity in m/s (not used)
+        0, 0, 0,                    # acceleration (not used)
+        0, 0                        # yaw, yaw_rate (not used)
+    )
+
+
+############## desired yaw function ##############  
+def condition_yaw(vehicle, yaw, relative=False):
+    if relative:
+        is_relative = 1 #yaw relative to direction of travel
+    else:
+        is_relative = 0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    vehicle.mav.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        yaw,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        0,          # param 3, direction -1 ccw, 1 cw, 0 for short turn
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+  
+
